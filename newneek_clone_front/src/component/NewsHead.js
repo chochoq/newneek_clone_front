a38f3e63873fbfe77b370dfc3804a593b8eb116a
illy <ma_natsu03@naver.com>
2021-04-03 22:07:34 +0900

    뉴스헤드와 언더라인 텍스트 스타일 추가

--- newneek_clone_front/src/component/NewsHead.js
+++ newneek_clone_front/src/component/NewsHead.js
@@ -1,6 +1,47 @@
 import React from "react";
-import { Grid, Text, Button } from "../elements";
+import "../shared/App.css";
+import styled from "styled-components";
+import { Text } from "../elements";
+
+import { history } from "../redux/configureStore";
 
 const NewsHead = (props) => {
-    
-}
+    return (
+        <Center>
+            <Text
+                color="#FB7800"
+                size="1rem"
+                bold
+                is_underline
+                margin="0"
+                _onClick={() => {
+                    history.push(`/postDetail/${props.id}`);
+                }}
+            >
+                {props.article.category}
+            </Text>
+            <Text size="2.3rem" letterSpacing="`0.75rem" bold padding="7px 0 28px">
+                {props.article.title}
+            </Text>
+            <Text size="1rem">{props.article.createdAt}</Text>
+        </Center>
+    );
+};
+
+const Center = styled.div`
+    text-align: center;
+    padding: 55px 55px 74px;
+    margin: 0 0 56px;
+    border-bottom: 1px solid #161616;
+`;
+
+NewsHead.defaultProps = {
+    article: {
+        id: "",
+        category: "경제",
+        title: "미국 경기 부양책 발표, 2260조 원짜리 부양책 💵",
+        createdAt: "2021/04/02 10:10:00",
+    },
+};
+
+export default NewsHead;
--- newneek_clone_front/src/elements/Text.js
+++ newneek_clone_front/src/elements/Text.js
@@ -2,13 +2,24 @@ import React from "react";
 import styled from "styled-components";
 
 const Text = (props) => {
-    const { bold, color, size, children, margin } = props;
+    const { bold, color, size, children, margin, letterSpacing, padding, is_underline } = props;
 
-    const styles = { bold: bold, color: color, size: size, margin: margin };
+    const styles = {
+        bold: bold,
+        color: color,
+        size: size,
+        margin: margin,
+        letterSpacing: letterSpacing,
+        padding: padding,
+    };
 
     return (
         <div>
-            <P {...styles}>{children}</P>
+            {is_underline ? (
+                <Underline {...styles}>{children}</Underline>
+            ) : (
+                <P {...styles}>{children}</P>
+            )}
         </div>
     );
 };
@@ -16,17 +27,31 @@ const Text = (props) => {
 Text.defaultProps = {
     children: null,
     bold: false,
-    color: "#333",
+    color: "#161616",
     size: "14px",
     margin: false,
+    padding: false,
+    underline: "none",
     _onClick: () => {},
 };
 
 const P = styled.p`
     color: ${(props) => props.color};
     font-size: ${(props) => props.size};
-    font-weight: ${(props) => (props.bold ? "600" : "400")};
-    ${(props) => (props.margin ? `margin: ${props.margin};` : "")};
+    font-weight: ${(props) => (props.bold ? "700" : "400")};
+    letter-spacing: ${(props) => props.letterSpacing};
+    ${(props) => (props.padding ? `margin: ${props.padding};` : "")};
+    ${(props) => (props.margin ? `padding: ${props.padding};` : "")};
+`;
+
+const Underline = styled.p`
+    color: ${(props) => props.color};
+    font-size: ${(props) => props.size};
+    font-weight: ${(props) => (props.bold ? "700" : "400")};
+    letter-spacing: ${(props) => props.letterSpacing};
+    text-decoration: underline;
+    ${(props) => (props.padding ? `margin: ${props.padding};` : "")};
+    ${(props) => (props.margin ? `padding: ${props.padding};` : "")};
 `;
 
 export default Text;
