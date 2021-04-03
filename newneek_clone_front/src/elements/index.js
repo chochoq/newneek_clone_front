35c02f04420f629f82372dba98a008358f0ed077
illy <ma_natsu03@naver.com>
2021-04-03 13:36:25 +0900

    엘리먼트

--- newneek_clone_front/src/elements/index.js
+++ newneek_clone_front/src/elements/index.js
@@ -1,9 +1,7 @@
-import React from "react";
 import Grid from "./Grid";
 import Image from "./Image";
 import Text from "./Text";
 import Input from "./Input";
 import Button from "./Button";
-import FaButton from "./FaButton";
 
-export { Grid, Image, Text, Input, Button, FaButton };
+export { Grid, Image, Text, Input, Button };
